f1393ac11ca0f95909d144bf1229d83a186a7b63
Joshua Lam <jlam@zendesk.com>
2017-07-18 08:22:41 -0700

    Use `waitUntil` instead of `waitForExist` in `count`
    Switches out `waitForExist` for `waitUntil`, since this is the expected
    behavior when working with wait times and asserting whether or not an
    element appears an expected amount of times  (i.e. when a wait is
    specified, the client will wait that amount of time for that element
    to be appear or not appear a certain number of times, as opposed to
    waiting for it to exist/not exist).

--- test/assertions/count-test.js
+++ test/assertions/count-test.js
@@ -1,93 +1,136 @@
 import chai, { expect } from 'chai';
 import sinonChai from 'sinon-chai';
 import FakeClient from '../stubs/fake-client';
-import proxyquire from 'proxyquire';
 import sinon from 'sinon';
+import count from '../../src/assertions/count';
 import immediately from '../../src/chains/immediately';
 
-const fakeClient = new FakeClient();
-
-const elementExists = sinon.stub();
-
-const count = proxyquire('../../src/assertions/count', {
-    '../util/element-exists': {
-        'default': elementExists
-    }
-}).default;
-
 //Using real chai, because it would be too much effort to stub/mock everything
-chai.use((chai, utils) => count(fakeClient, chai, utils));
-chai.use((chai, utils) => immediately(fakeClient, chai, utils));
-
 chai.use(sinonChai);
 
+const doesNotHaveCountError = (count, actualCount, defaultWait=0) => {
+    return 'Element with selector .some-selector does not ' +
+        `appear in the DOM ${count} times within ${defaultWait} ms, ` +
+        `but it shows up ${actualCount} times instead.`;
+};
+
+const hasCountError = (count, defaultWait=0) => {
+    return 'Element with selector .some-selector still ' +
+        `appears in the DOM ${count} times after ${defaultWait} ms`;
+};
+
 describe('count', () => {
+    let elements
+    let fakeClient;
+
     beforeEach(() => {
-        fakeClient.__resetStubs__();
-        elementExists.reset();
+        elements = ['foo', 'bar'];
+        fakeClient = new FakeClient();
+
+        fakeClient.elements.throws('ArgumentError');
+        fakeClient.elements.withArgs('.some-selector').returns({value: elements});
+
+        fakeClient.waitUntil.callsFake((condition, timeout, timeoutMsg) => {
+            if (condition()) return;
+
+            throw new Error(timeoutMsg);
+        });
+
+        chai.use((chai, utils) => count(fakeClient, chai, utils));
+        chai.use((chai, utils) => immediately(fakeClient, chai, utils));
     });
 
+    afterEach(() => fakeClient.__resetStubs__());
+
     describe('When in synchronous mode', () => {
-        it('Should throw element doesn\'t exist error', () => {
-            const testError = 'foobar';
-            elementExists.throws(new Error(testError));
-            expect(() => expect('.some-selector').to.have.count(123)).to.throw(testError);
-            expect(elementExists).to.have.been.calledOnce;
+        describe('When not negated', () => {
+            beforeEach(() => {
+                expect('.some-selector').to.have.count(elements.length);
+            });
+
+            it('Should call `waitUntil`', () => {
+                expect(fakeClient.waitUntil).to.have.been.calledWithMatch(
+                    callback => callback(),
+                    0
+                );
+            });
+
+            describe('When the element still does not appear the expected times after the wait time', () => {
+                it('Should throw an exception', () => {
+                    expect(() => expect('.some-selector').to.have.count(elements.length + 1))
+                        .to.throw(doesNotHaveCountError(elements.length + 1, elements.length));
+                });
+            });
         });
 
         describe('When negated', () => {
-            beforeEach(() => fakeClient.elements.returns({value: []}));
+            beforeEach(() => {
+                expect('.some-selector').to.not.have.count(elements.length + 1);
+            });
 
-            it('Should call element exists with reverse=false', () => {
-                expect('.some-selector').to.not.have.count(123);
-                expect(elementExists).to.have.been.calledWith(fakeClient, '.some-selector', 0, false);
+            it('Should call `waitUntil`', () => {
+                expect(fakeClient.waitUntil).to.have.been.calledWithMatch(
+                    callback => callback(),
+                    0,
+                    hasCountError(elements.length + 1)
+                );
+            });
+
+            describe('When the element still appears the expected times after the wait time', () => {
+                it('Should throw an exception', () => {
+                    expect(() => expect('.some-selector').to.not.have.count(elements.length))
+                        .to.throw(hasCountError(elements.length));
+                });
             });
         });
 
-        describe('When element exists', () => {
-            describe('When call is chained with Immediately', () => {
-                let testResult;
+        describe('When the element count matches the expectation', () => {
+            it('Should not throw an exception', () => {
+                expect('.some-selector').to.have.count(elements.length);
+            });
+
+            describe('When given a default wait time' , () => {
                 beforeEach(() => {
-                    testResult = [];
-                    elementExists.returns();
-                    fakeClient.elements.returns({value: testResult});
-                    testResult.push('foo');
-                    testResult.push('bar');
+                  chai.use((chai, utils) => count(fakeClient, chai, utils, {defaultWait: 100}));
+
+                  expect('.some-selector').to.have.count(elements.length);
                 });
 
-                it('Should not throw an exception', () => {
-                    expect('.some-selector').to.have.immediately().count(testResult.length);
-                });
-
-                it('Should not wait till the element exists', () => {
-                    expect('.some-selector').to.have.immediately().count(testResult.length);
-                    expect(elementExists).to.not.have.been.called;
-                });
-
-                describe('When negated', () => {
-                    it('Should throw an error', () => {
-                        expect(() => expect('.some-selector').to.not.have.immediately().count(testResult.length)).to.throw();
-                    });
+                it('Should call `waitUntil` with the specified wait time', () => {
+                    expect(fakeClient.waitUntil).to.have.been.calledWithMatch(
+                        callback => callback(),
+                        100
+                    );
                 });
             });
 
-            describe('When element count matches expectation', () => {
-                let testResult;
+            describe('When the call is chained with `immediately`', () => {
                 beforeEach(() => {
-                    testResult = [];
-                    elementExists.returns();
-                    fakeClient.elements.returns({value: testResult});
-                    testResult.push('foo');
-                    testResult.push('bar');
+                    expect('.some-selector').to.have.immediately().count(elements.length);
                 });
 
-                it('Should not throw an exception', () => {
-                    expect('.some-selector').to.have.count(testResult.length);
+                it('Should not wait for the element to match the expected count', () => {
+                    expect(fakeClient.waitUntil).to.not.have.been.called;
                 });
-                describe('When negated', () => {
-                    it('Should throw an error', () => {
-                        expect(() => expect('.some-selector').to.not.have.count(testResult.length)).to.throw();
-                    });
+            });
+
+            describe('When the assertion is negated', () => {
+                it('Should throw an exception', () => {
+                    expect(() => expect('.some-selector').to.not.have.count(elements.length))
+                        .to.throw();
+                });
+            });
+        });
+
+        describe('When the element count does not match the expectation', () => {
+            it('Should throw an exception', () => {
+                expect(() => expect('.some-selector').to.have.count(elements.length + 1))
+                    .to.throw();
+            });
+
+            describe('When the assertion is negated', () => {
+                it('Should not throw an exception', () => {
+                    expect('.some-selector').to.not.have.count(elements.length + 1);
                 });
             });
         });
